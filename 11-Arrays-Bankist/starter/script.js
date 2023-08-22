a309aa0e1afa15f1876335772d2258d127f4f4e2
MaRage <marat_gadzhiev_2003@mail.ru>
2023-08-23 02:37:06 +0300

    В процессе

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -155,6 +155,23 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+/////////////////////////////////////////////////
 //!Coding Challenge #1
 
 /* 
@@ -193,22 +210,89 @@ GOOD LUCK 😀
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
-
+/////////////////////////////////////////////////
 //!The Map Method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const eurToUsd = 1.1;
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
 
 // const movementsUSD = movements.map(function (mov) {
 //   return mov * eurToUsd;
 // });
 
-const movementsUSD = movements.map(mov => mov * eurToUsd);
-console.log(movementsUSD);
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movementsUSD);
 
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
-      mov
-    )} `
-);
-console.log(movementsDescriptions);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )} `
+// );
+// console.log(movementsDescriptions);
+/////////////////////////////////////////////////
+//!The Filter Method
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// const withdraws = movements.filter(num => num < 0);
+// console.log(deposits);
+// console.log(withdraws);
+/////////////////////////////////////////////////
+//!The Reduce Method
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, current, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + current;
+// }, 0);
+
+// const balance2 = movements.reduce((acc, cur) => acc + cur);
+// console.log(balance2);
+
+// // Maximum value
+// const max = movements.reduce((acc, cur) => {
+//   if (acc > cur) return acc;
+//   else return cur;
+// }, movements[0]);
+// console.log(max);
+/////////////////////////////////////////////////
+//! Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// const calcAverageHumanAge = function (ages) {
+//   //*1 Пункт
+//   const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   console.log(humanAge);
+//   //*2 Пункт
+//   const adultDogs = humanAge.filter(age => age >= 18);
+//   console.log(adultDogs);
+//   //*3 Пункт
+//   const averageAge =
+//     adultDogs.reduce((acc, cur) => acc + cur, 0) / adultDogs.length;
+//   console.log(averageAge);
+//   //*Всё вместе
+//   return ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+// };
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
