315a7d3ae4c888a202b496d556e6d47c1fe06369
MaRage <marat_gadzhiev_2003@mail.ru>
2024-02-23 00:41:11 +0300

    Пройдено

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -286,6 +286,7 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+//Sorting movements
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
